b4280c70e848f49715c8d95eeab268ed2c1199ab
Suzana V <suzana@vukovic.ca>
2020-08-31 17:03:37 -0400

    Update csvToJson.py

--- csvToJson.py
+++ csvToJson.py
@@ -2,14 +2,14 @@ import csv
 import json
 
 my_list = []
-with open('HLR-FPSmokeAlarm.csv') as csvfile:
+with open('test.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        index = row["Assignment of Index"].replace(" " , "")
-        message = row["Fault Message Banner Wording"]
-        my_dict = {"Index":index, "Message": message}   
+        index = row["name"].replace(" " , "")
+        message = row["age"]
+        my_dict = {"Name":name, "Age": age}   
         my_list.append(my_dict)
 
-with open('FPSmokeAlarm.json', 'w') as outfile:
+with open('testing.json', 'w') as outfile:
     json.dump(my_list, outfile, indent= 4)
 
